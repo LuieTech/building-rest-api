5bc0d0dab5167c68c37592347ffa98c7f19195e8
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-14 18:30:39 -0400

    chore: "Implement routing & connection to database"

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ require("dotenv").config()
 const express = require('express');
 const createError = require("http-errors")
 const logger = require('morgan')
+
+require("./configs/db.config")
+
 const app = express()
 
 app.use(express.json())
 app.use(logger('combined'))
+
+const api = require('./configs/routes.config')
+app.use('/v1', api)
 
 const PORT = process.env.PORT || 3000
 
