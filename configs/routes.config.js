5f66132fa8b40caa1bac660fcaa5f3a0137b5236
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-15 19:10:11 -0400

    chore:"implement Author CRUD pending UPDATE"

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router()
 const author = require("../controllers/author.controller")
 
-router.post("/author-create", author.create)
+router.get("/authors", author.list)
+router.post("/authors", author.create)
+router.get("/authors/:id", author.detail)
+router.delete("/authors/:id", author.delete)
 
 module.exports = router
