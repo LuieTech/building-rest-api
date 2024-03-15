7f5319ac8ca3c792e4600dcf0db415135f80f72b
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-15 19:10:33 -0400

    chore:"Implement Author CRUD controllers"

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,3 +1,4 @@
+const createError = require("http-errors")
 const Author = require("../models/author.model")
 
 module.exports.create = (req, res, next) => {
@@ -5,5 +6,41 @@ module.exports.create = (req, res, next) => {
   Author.create(req.body)
     .then(author => res.status(201).json(author))
     .catch(error => next(error))
+
+}
+
+module.exports.list = (req, res, next) => {
+
+  const criteria = Object.keys(req.query).reduce((newQueryObject, key) => {
+    newQueryObject[key] = req.query[key];
+    return newQueryObject;
+  }, {})
+
+  Author.find(criteria)
+    .then(authors => res.status(200).json(authors))
+    .catch(error => next(error))
+
+}
+
+module.exports.detail = (req, res, next) => {
+
+  Author.findById(req.params.id)
+    .then(author => {
+      if(!author) next(createError(404, "Author not found"))
+      else res.status(200).json(author)
+    })
+    .catch(error => next(error))
+
+}
+
+module.exports.delete = (req, res, next) => {
+
+  Author.findById(req.params.id)
+   .then((author) => {
+    if(!author) next(createError(404, "Author not found"))
+    else return Author.deleteOne({_id: req.params.id})
+          .then(author => res.status(204).send())
+   })
+   .catch(error => next(error))
 
 }
