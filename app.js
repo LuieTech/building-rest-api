cd27d900b070cf639b980b3cdb2078b56bf5205e
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-17 11:28:00 -0400

    chore: "Implement general error-handling middleware in the app.js for all type of errors"

--- app.js
+++ app.js
@@ -10,8 +10,42 @@ const app = express()
 app.use(express.json())
 app.use(logger('dev'))
 
-const api = require('./configs/routes.config')
+const api = require('./configs/routes.config');
+const { default: mongoose } = require("mongoose");
+const { create } = require("./controllers/author.controller");
 app.use('/v1', api)
+
+app.use((req, res, next) => next(createError(404, "Route not found")));
+
+app.use((error, req, res, next) => {
+
+  if (error instanceof mongoose.Error.CastError && error.message.includes("_id")) {
+      error = createError(404, "Resource not found")
+    }
+  else if (error instanceof mongoose.Error.ValidationError) {
+      error = createError(400, error)
+  }
+  else if (!error.status) {
+      error = createError(500, error)
+  }
+
+  let errors;
+  if(error.errors) {
+    errors = Object.keys(error.errors)
+      .reduce((newObject, currentKey) => {
+        newObject[currentKey] = error.errors[currentKey].message || error.errors[currentKey]
+        return newObject
+      }, {})
+  }
+
+  const data = {
+    message: error.message,
+    errors
+  }
+
+  res.status(error.status).json(data)
+
+})
 
 const PORT = process.env.PORT || 3000
 
