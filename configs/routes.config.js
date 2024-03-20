c6ec4fd6f61278f473b8daa66a4b215d9f9adf78
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2024-03-20 13:28:11 -0400

    chore:"implement Book path routes for create and delete"

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router()
 const author = require("../controllers/author.controller")
+const book = require("../controllers/book.controller")
 
+//Author routes
 router.get("/authors", author.list)
 router.post("/authors", author.create)
 router.get("/authors/:id", author.detail)
 router.delete("/authors/:id", author.delete)
+
+//Book routes
+
+router.post("/author/:authorId/books", book.create)
+router.delete("/books/:id", book.delete)
 
 module.exports = router
